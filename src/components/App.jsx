17c4d758b2db89f0fe2667c2c4fe50bcaea5a351
dracero <dracero@fi.uba.ar>
2023-11-09 20:09:37 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import Move from "./Move";
+import { OrbitControls} from "@react-three/drei";
+import { Canvas } from '@react-three/fiber'
+//import { Physics} from "@react-three/cannon"
+
+
+const App  = () => {
+   
+  
+  return (  
+  <div className="boardCanvas"
+  style={{ width: "100vw", height: "40vw" }} > 
+  <Canvas referenceSpace="local">
+    <hemisphereLight intensity={1} />
+      <OrbitControls />
+       <Move position={[0, -1, -9]} rotation={[0, -1.85, 0]}/>
+  </Canvas> 
+</div>
+
+  );
+}
+
+export default App
---
+++ src/components/test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const HolaMundo = () => {
+  return (
+    <h1>Hola, mundo!</h1>
+  );
+}
+
+export default HolaMundo;
